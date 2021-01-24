650610d866ffb64194eb8ba63be5719cda27bcb0
Humberto Sampaio <humberto.sampaio@guiando.com.br>
2021-01-24 19:36:02 -0300

    Separação da criação de cada parte do carro

--- works/basicScene.js
+++ works/basicScene.js
@@ -2,158 +2,199 @@ function Kart() {
 	var material = new THREE.MeshPhongMaterial({ color: 'rgb(100,255,100)' });
 	var matrizRotacao = new THREE.Matrix4();
 
-	// Corpo principal
-	const tamanhoCorpoPrincipal = { x: 2.5, y: 3.5, z: 0.7 };
-	var corpoPrincipal = criarParalelogramo(tamanhoCorpoPrincipal);
-
-	// Bico
-	var tamanhoBico = { x: tamanhoCorpoPrincipal.z, y: tamanhoCorpoPrincipal.y * 0.5, z: tamanhoCorpoPrincipal.z };
-	var bico = criarParalelogramo(tamanhoBico);
-	bico.translateY((tamanhoCorpoPrincipal.y * 0.5) + (tamanhoBico.y * 0.5));
-
-	// Asa dianteira
-	var tamanhoAsaDianteira = { x: tamanhoCorpoPrincipal.x, y: tamanhoBico.y * 0.3, z: tamanhoBico.z * 0.5 };
-	var asaDianteira = criarParalelogramo(tamanhoAsaDianteira);
-	asaDianteira
-		.translateY((tamanhoBico.y * 0.5) - (tamanhoAsaDianteira.y * 0.5))
-		.translateZ(tamanhoBico.z * -0.25);
-
-	// Ligação entre o corpo principal e a asa traseira
+	var tamanhoCockpit = { x: 2.5, y: 3.5, z: 0.7 };
+	var tamanhoBico = { x: tamanhoCockpit.z, y: tamanhoCockpit.y * 0.5, z: tamanhoCockpit.z };
+	var tamanhoAsaDianteira = { x: tamanhoCockpit.x, y: tamanhoBico.y * 0.3, z: tamanhoBico.z * 0.5 };
 	var tamanhoLigacaoTraseira = { x: tamanhoBico.x, y: tamanhoBico.y * 0.5, z: tamanhoBico.z };
-	var ligacaoTraseira = criarParalelogramo(tamanhoLigacaoTraseira);
-	ligacaoTraseira.translateY((tamanhoCorpoPrincipal.y * -0.5) + (tamanhoLigacaoTraseira.y * -0.5));
-
-	// Bases da asa traseira
 	var tamanhoBaseAsaTraseira = { x: tamanhoLigacaoTraseira.x * 0.05, y: tamanhoLigacaoTraseira.y * 0.5, z: tamanhoLigacaoTraseira.z * 0.5 };
-
-	var baseDireitaAsaTraseira = criarParalelogramo(tamanhoBaseAsaTraseira);
-	baseDireitaAsaTraseira.translateX((tamanhoLigacaoTraseira.x * 0.25))
-		.translateZ((tamanhoLigacaoTraseira.z + tamanhoBaseAsaTraseira.z) * 0.5);
-
-	var baseEsquerdaAsaTraseira = criarParalelogramo(tamanhoBaseAsaTraseira);
-	baseEsquerdaAsaTraseira.translateX((tamanhoLigacaoTraseira.x * -0.25))
-		.translateZ((tamanhoLigacaoTraseira.z + tamanhoBaseAsaTraseira.z) * 0.5);
-
-	// Asa traseira (aerofólio)
-	var tamanhoAsaTraseira = { x: (tamanhoCorpoPrincipal.x - tamanhoLigacaoTraseira.x) * 1.2, y: tamanhoBaseAsaTraseira.y, z: tamanhoBaseAsaTraseira.x };
-	var asaTraseira = criarParalelogramo(tamanhoAsaTraseira);
-	asaTraseira.translateZ((tamanhoBaseAsaTraseira.z) + ((tamanhoLigacaoTraseira.z + tamanhoAsaTraseira.z) * 0.5));
-
-	// Eixos pneus
+	var tamanhoAsaTraseira = { x: (tamanhoCockpit.x - tamanhoLigacaoTraseira.x) * 1.2, y: tamanhoBaseAsaTraseira.y, z: tamanhoBaseAsaTraseira.x };
 	var tamanhoEixoPneu = { raio: tamanhoBico.z * 0.15, altura: ((tamanhoAsaDianteira.x - tamanhoBico.x) * 0.5) };
-
-	// 	->	Eixo dianteiro direito
-	var eixoPneuDianteiroDireito = criarCilindro(tamanhoEixoPneu);
-	eixoPneuDianteiroDireito.matrix.multiply(
-		matrizRotacao.makeTranslation(
-			(tamanhoBico.x + tamanhoEixoPneu.altura) * 0.5,
-			tamanhoAsaDianteira.y * -0.5,
-			0
-		)
-	);
-	eixoPneuDianteiroDireito.matrix.multiply(matrizRotacao.makeRotationZ(degreesToRadians(90)));
-
-	// 	->	Eixo dianteiro esquerdo
-	var eixoPneuDianteiroEsquerdo = criarCilindro(tamanhoEixoPneu);
-	eixoPneuDianteiroEsquerdo.matrix.multiply(
-		matrizRotacao.makeTranslation(
-			(tamanhoBico.x + tamanhoEixoPneu.altura) * -0.5,
-			tamanhoAsaDianteira.y * -0.5,
-			0
-		)
-	);
-	eixoPneuDianteiroEsquerdo.matrix.multiply(matrizRotacao.makeRotationZ(degreesToRadians(90)));
-
-	// 	->	Eixo traseiro direito
-	var eixoPneuTraseiroDireito = criarCilindro(tamanhoEixoPneu);
-	eixoPneuTraseiroDireito.matrix.multiply(
-		matrizRotacao.makeTranslation(
-			(tamanhoBico.x + tamanhoEixoPneu.altura) * 0.5,
-			0,
-			0
-		)
-	);
-	eixoPneuTraseiroDireito.matrix.multiply(matrizRotacao.makeRotationZ(degreesToRadians(90)));
-
-	// 	->	Eixo traseiro esquerdo
-	var eixoPneuTraseiroEsquerdo = criarCilindro(tamanhoEixoPneu);
-	eixoPneuTraseiroEsquerdo.matrix.multiply(
-		matrizRotacao.makeTranslation(
-			(tamanhoBico.x + tamanhoEixoPneu.altura) * -0.5,
-			0,
-			0
-		)
-	);
-	eixoPneuTraseiroEsquerdo.matrix.multiply(matrizRotacao.makeRotationZ(degreesToRadians(90)));
-
-	// Pneus
 	var tamanhoPneu = { raioTorus: 0.3, raioTubo: tamanhoEixoPneu.altura * 0.25 };
 
-	// -> 	Pneu Dianteiro Direito
-	var pneuDianteiroDireito = criarTorus(tamanhoPneu);
-	pneuDianteiroDireito.matrix.multiply(
-		matrizRotacao.makeTranslation(
-			0,
-			tamanhoEixoPneu.altura * -0.5,
-			0
-		)
-	);
-	pneuDianteiroDireito.matrix.multiply(matrizRotacao.makeRotationX(degreesToRadians(90)));
+	var cockpit = criarCockpit();
+	var bico = criarBico();
+	var asaDianteira = criarAsaDianteira();
+	var ligacaoTraseira = criarLigacaoTraseira();
+	var baseDireitaAsaTraseira = criarBaseDireitaAsaTraseira();
+	var baseEsquerdaAsaTraseira = criarBaseEsquerdaAsaTraseira();
+	var asaTraseira = criarAsaTraseira();
 
-	// -> 	Pneu Dianteiro Esquerdo
-	var pneuDianteiroEsquerdo = criarTorus(tamanhoPneu);
-	pneuDianteiroEsquerdo.matrix.multiply(
-		matrizRotacao.makeTranslation(
-			0,
-			tamanhoEixoPneu.altura * 0.5,
-			0
-		)
-	);
-	pneuDianteiroEsquerdo.matrix.multiply(matrizRotacao.makeRotationX(degreesToRadians(90)));
+	// Eixos dos pneus
+	var eixoPneuDianteiroDireito = criarEixoPneuDianteiroDireito();
+	var eixoPneuDianteiroEsquerdo = criarEixoPneuDianteiroEsquerdo();
+	var eixoPneuTraseiroDireito = criarEixoPneuTraseiroDireito();
+	var eixoPneuTraseiroEsquerdo = criarEixoPneuTraseiroEsquerdo();
 
-	// 	->	Eixo Traseiro direito
-	var pneuTraseiroDireito = criarTorus(tamanhoPneu);
-	pneuTraseiroDireito.matrix.multiply(
-		matrizRotacao.makeTranslation(
-			0,
-			tamanhoEixoPneu.altura * -0.5,
-			0
-		)
-	);
-	pneuTraseiroDireito.matrix.multiply(matrizRotacao.makeRotationX(degreesToRadians(90)));
+	// Pneus
+	var pneuDianteiroDireito = criarPneuDianteiroDireito();
+	var pneuDianteiroEsquerdo = criarPneuDianteiroEsquerdo();
+	var pneuTraseiroDireito = criarPneuTraseiroDireito();
+	var pneuTraseiroEsquerdo = criarPneuTraseiroEsquerdo();
 
-	// -> 	Pneu Traseiro Esquerdo
-	var pneuTraseiroEsquerdo = criarTorus(tamanhoPneu);
-	pneuTraseiroEsquerdo.matrix.multiply(
-		matrizRotacao.makeTranslation(
-			0,
-			tamanhoEixoPneu.altura * 0.5,
-			0
-		)
-	);
-	pneuTraseiroEsquerdo.matrix.multiply(matrizRotacao.makeRotationX(degreesToRadians(90)));
-
-	corpoPrincipal.add(bico);
+	cockpit.add(bico);
 	bico.add(asaDianteira);
 	bico.add(eixoPneuDianteiroDireito);
 	bico.add(eixoPneuDianteiroEsquerdo);
 	eixoPneuDianteiroDireito.add(pneuDianteiroDireito);
 	eixoPneuDianteiroEsquerdo.add(pneuDianteiroEsquerdo);
 
-	corpoPrincipal.add(ligacaoTraseira);
+	cockpit.add(ligacaoTraseira);
 	ligacaoTraseira.add(eixoPneuTraseiroDireito);
 	ligacaoTraseira.add(eixoPneuTraseiroEsquerdo);
+	eixoPneuTraseiroDireito.add(pneuTraseiroDireito);
+	eixoPneuTraseiroEsquerdo.add(pneuTraseiroEsquerdo);
 	ligacaoTraseira.add(baseDireitaAsaTraseira);
 	ligacaoTraseira.add(baseEsquerdaAsaTraseira);
 	ligacaoTraseira.add(asaTraseira);
-	eixoPneuTraseiroDireito.add(pneuTraseiroDireito);
-	eixoPneuTraseiroEsquerdo.add(pneuTraseiroEsquerdo);
 
-	corpoPrincipal.position.set(0.0, 0.0, (tamanhoCorpoPrincipal.z / 2) + 0.15);
+	cockpit.position.set(0.0, 0.0, (tamanhoCockpit.z / 2) + 0.15);
 
-	return corpoPrincipal;
+	return cockpit;
 
-	function criarParalelogramo(tamanho) {
+	// Funções auxiliares para criação dos sólidos
+	function criarCockpit() {
+		return criarParalelepípedo(tamanhoCockpit);
+	}
+
+	function criarBico() {
+		return criarParalelepípedo(tamanhoBico)
+			.translateY((tamanhoCockpit.y * 0.5) + (tamanhoBico.y * 0.5));
+	}
+
+	function criarAsaDianteira() {
+		return criarParalelepípedo(tamanhoAsaDianteira)
+			.translateY((tamanhoBico.y * 0.5) - (tamanhoAsaDianteira.y * 0.5))
+			.translateZ(tamanhoBico.z * -0.25);
+	}
+
+	function criarLigacaoTraseira() {
+		return criarParalelepípedo(tamanhoLigacaoTraseira)
+			.translateY((tamanhoCockpit.y * -0.5) + (tamanhoLigacaoTraseira.y * -0.5));
+	}
+
+	function criarBaseDireitaAsaTraseira() {
+		return criarParalelepípedo(tamanhoBaseAsaTraseira)
+			.translateX((tamanhoLigacaoTraseira.x * 0.25))
+			.translateZ((tamanhoLigacaoTraseira.z + tamanhoBaseAsaTraseira.z) * 0.5);
+	}
+
+	function criarBaseEsquerdaAsaTraseira() {
+		return criarParalelepípedo(tamanhoBaseAsaTraseira)
+			.translateX((tamanhoLigacaoTraseira.x * -0.25))
+			.translateZ((tamanhoLigacaoTraseira.z + tamanhoBaseAsaTraseira.z) * 0.5);
+	}
+
+	function criarAsaTraseira() {
+		return criarParalelepípedo(tamanhoAsaTraseira)
+			.translateZ((tamanhoBaseAsaTraseira.z) + ((tamanhoLigacaoTraseira.z + tamanhoAsaTraseira.z) * 0.5));
+	}
+
+	function criarEixoPneuDianteiroDireito() {
+		var eixoPneuDianteiroDireito = criarCilindro(tamanhoEixoPneu);
+		eixoPneuDianteiroDireito.matrix.multiply(
+			matrizRotacao.makeTranslation(
+				(tamanhoBico.x + tamanhoEixoPneu.altura) * 0.5,
+				tamanhoAsaDianteira.y * -0.5,
+				0
+			)
+		);
+		eixoPneuDianteiroDireito.matrix.multiply(matrizRotacao.makeRotationZ(degreesToRadians(90)));
+		return eixoPneuDianteiroDireito;
+	}
+
+	function criarEixoPneuDianteiroEsquerdo() {
+		var eixoPneuDianteiroEsquerdo = criarCilindro(tamanhoEixoPneu);
+		eixoPneuDianteiroEsquerdo.matrix.multiply(
+			matrizRotacao.makeTranslation(
+				(tamanhoBico.x + tamanhoEixoPneu.altura) * -0.5,
+				tamanhoAsaDianteira.y * -0.5,
+				0
+			)
+		);
+		eixoPneuDianteiroEsquerdo.matrix.multiply(matrizRotacao.makeRotationZ(degreesToRadians(90)));
+		return eixoPneuDianteiroEsquerdo;
+	}
+
+	function criarEixoPneuTraseiroDireito() {
+		var eixoPneuTraseiroDireito = criarCilindro(tamanhoEixoPneu);
+		eixoPneuTraseiroDireito.matrix.multiply(
+			matrizRotacao.makeTranslation(
+				(tamanhoBico.x + tamanhoEixoPneu.altura) * 0.5,
+				0,
+				0
+			)
+		);
+		eixoPneuTraseiroDireito.matrix.multiply(matrizRotacao.makeRotationZ(degreesToRadians(90)));
+		return eixoPneuTraseiroDireito;
+	}
+
+	function criarEixoPneuTraseiroEsquerdo() {
+		var eixoPneuTraseiroEsquerdo = criarCilindro(tamanhoEixoPneu);
+		eixoPneuTraseiroEsquerdo.matrix.multiply(
+			matrizRotacao.makeTranslation(
+				(tamanhoBico.x + tamanhoEixoPneu.altura) * -0.5,
+				0,
+				0
+			)
+		);
+		eixoPneuTraseiroEsquerdo.matrix.multiply(matrizRotacao.makeRotationZ(degreesToRadians(90)));
+		return eixoPneuTraseiroEsquerdo;
+	}
+
+	function criarPneuDianteiroDireito() {
+		var pneuDianteiroDireito = criarTorus(tamanhoPneu);
+		pneuDianteiroDireito.matrix.multiply(
+			matrizRotacao.makeTranslation(
+				0,
+				tamanhoEixoPneu.altura * -0.5,
+				0
+			)
+		);
+		pneuDianteiroDireito.matrix.multiply(matrizRotacao.makeRotationX(degreesToRadians(90)));
+		return pneuDianteiroDireito;
+	}
+
+	function criarPneuDianteiroEsquerdo() {
+		var pneuDianteiroEsquerdo = criarTorus(tamanhoPneu);
+		pneuDianteiroEsquerdo.matrix.multiply(
+			matrizRotacao.makeTranslation(
+				0,
+				tamanhoEixoPneu.altura * 0.5,
+				0
+			)
+		);
+		pneuDianteiroEsquerdo.matrix.multiply(matrizRotacao.makeRotationX(degreesToRadians(90)));
+		return pneuDianteiroEsquerdo;
+	}
+
+	function criarPneuTraseiroDireito() {
+		var pneuTraseiroDireito = criarTorus(tamanhoPneu);
+		pneuTraseiroDireito.matrix.multiply(
+			matrizRotacao.makeTranslation(
+				0,
+				tamanhoEixoPneu.altura * -0.5,
+				0
+			)
+		);
+		pneuTraseiroDireito.matrix.multiply(matrizRotacao.makeRotationX(degreesToRadians(90)));
+		return pneuTraseiroDireito;
+	}
+
+	function criarPneuTraseiroEsquerdo() {
+		var pneuTraseiroEsquerdo = criarTorus(tamanhoPneu);
+		pneuTraseiroEsquerdo.matrix.multiply(
+			matrizRotacao.makeTranslation(
+				0,
+				tamanhoEixoPneu.altura * 0.5,
+				0
+			)
+		);
+		pneuTraseiroEsquerdo.matrix.multiply(matrizRotacao.makeRotationX(degreesToRadians(90)));
+		return pneuTraseiroEsquerdo;
+	}
+
+	// Funções de criação genéricas
+	function criarParalelepípedo(tamanho) {
 		var geometria = new THREE.BoxGeometry(tamanho.x, tamanho.y, tamanho.z);
 		return new THREE.Mesh(geometria, material);
 	}
